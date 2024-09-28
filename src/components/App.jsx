35f16a4b5dc15ae49e4a974b103e105a0b969aea
KlaudiaKowalska <150347636+KlaudiaKowalska@users.noreply.github.com>
2024-09-28 14:20:42 +0200

    1420

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux'; // Importuj useSelector
 import PrivateRoute from './PrivateRout/PrivateRout';
 import { Layout } from './Layout/Layout';
-import LoginPage from '../pages/LoginPage';
+import LoginPage from '../pages/LoginPage'; // Zaktualizuj import do LoginPage
 const Home = lazy(() => import('../pages/Home'));
 const Register = lazy(() => import('./Register/Register.jsx'));
 
