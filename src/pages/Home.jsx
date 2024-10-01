194e5abdf06e6b094a9787dcb154c5aa1904fc34
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-10-01 10:53:14 +0200

    Back to home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,43 @@
+import { BackToHome } from 'components/BackToHome/BackToHome';
 import Balans from 'components/Balans/Balans';
 import ExpensesButton from 'components/ExpensesButtons/ExpensesButtons';
+import HomeInput from 'components/HomeInput/HomeInput';
 import TableIncomeExpenses from 'components/TableIncomeExpenses/TableIncomeExpenses';
 import { ToTransaction } from 'components/ToTransaction/ToTransaction';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <ToTransaction />
-      <Balans />
-      <ExpensesButton />
-      <TableIncomeExpenses />
-    </div>
-  );
+  const [transaction, setTransaction] = useState('home');
+
+  const handleClick = () => {
+    if (transaction === 'home') {
+      setTransaction('transaction');
+    }
+    if (transaction === 'transaction') {
+      setTransaction('home');
+    }
+
+    console.log('klik!');
+  };
+
+  if (transaction === 'home') {
+    return (
+      <>
+        <ToTransaction handler={handleClick} />
+        <Balans />
+        <ExpensesButton />
+
+        <TableIncomeExpenses />
+      </>
+    );
+  }
+  if (transaction === 'transaction')
+    return (
+      <>
+        <BackToHome home={handleClick} />
+        <HomeInput />
+      </>
+    );
 };
 
 export default Home;
