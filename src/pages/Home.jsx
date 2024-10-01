8a7e3f178be324061a82effef31d5ff86fd6e8ce
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-10-01 14:40:41 +0200

    resize listener

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
 import { BackToHome } from 'components/BackToHome/BackToHome';
 import Balans from 'components/Balans/Balans';
 import ExpensesButton from 'components/ExpensesButtons/ExpensesButtons';
-import { HomeInput } from 'components/HomeInput/HomeInput';
 import { ExpensesTable } from 'components/ExpensesTable/ExpensesTable';
 import { ToTransaction } from 'components/ToTransaction/ToTransaction';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import MobilePage from 'components/MobilePage/MobilePage';
 
 const Home = () => {
   const [transaction, setTransaction] = useState('home');
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 768) {
+        setTransaction('home');
+      }
+    }
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const handleClick = () => {
     if (transaction === 'home') {
@@ -34,7 +48,7 @@ const Home = () => {
     return (
       <>
         <BackToHome home={handleClick} />
-        <HomeInput />
+        <MobilePage />
       </>
     );
 };
